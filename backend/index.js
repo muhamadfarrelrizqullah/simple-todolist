5340108a95e57ef681ffeb62e79c86f410e945a4
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-09-02 12:47:00 +0700

    feat: delete

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,14 @@ app.put('/update/:id', (req, res) => {
         .catch(err => res.json(err));
 });
 
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+
+    TodoModel.findByIdAndDelete(id)
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
 
 app.listen(3001, () => {
     console.log("Server is running..")
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -15,9 +15,21 @@ function Home() {
 
     const handleEdit = (id) => {
         axios.put('http://localhost:3001/update/' + id)
-            .then(result => console.log(result))
+            .then(result => {
+              setTodos(todos.map(todo =>
+                todo._id === id ? { ...todo, done: !todo.done } : todo
+              ));
+            })
             .catch(err => console.log(err));
     };  
+
+    const handleDelete = (id) => {
+      axios.delete(`http://localhost:3001/delete/${id}`)
+          .then(result => {
+              setTodos(todos.filter(todo => todo._id !== id));
+          })
+          .catch(err => console.log(err));
+    };
 
     return (
         <div className="home">
@@ -40,7 +52,7 @@ function Home() {
                             <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
                           </div>
                           <div>
-                            <span><BsFillTrashFill className='icon' /></span>
+                            <span><BsFillTrashFill className='icon' onClick={() => handleDelete(todo._id)} /></span>
                           </div>
                         </div>
                     ))
