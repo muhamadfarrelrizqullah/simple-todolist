309375f44a7af403ebc50fabeafc3e89591e1881
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-09-07 11:36:00 +0700

    fix: create data

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
 
 app.post('/add', (req, res) => {
     const task = req.body.task;
+    if (!task) {
+        return res.status(400).json({ error: "Task is required" });
+    }
     TodoModel.create({
         task: task
     }).then(result => res.json(result))
--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 function Create() {
   const [task, setTask] = useState()
